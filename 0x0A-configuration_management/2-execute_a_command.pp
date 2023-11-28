75ba3596c83e908b432f410bf60b6f6d6216cf08
Osman <oselibas@gmail.com>
2023-11-28 08:52:44 -0800

    file updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-# This manifest installs a package, flask
+# This manifest kills a process
 
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+exec {'pkill':
+    command => '/usr/bin/pkill killmenow'
 }
